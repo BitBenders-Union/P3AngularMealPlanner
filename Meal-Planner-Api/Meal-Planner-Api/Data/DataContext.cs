0cf30b61f805f579cee57e051e2b2a63f7d871fd
Jonas Østergaard <Jonasoe@hotmail.com>
2023-09-04 10:39:54 +0200

    DataContext Added

--- Meal-Planner-Api/Meal-Planner-Api/Data/DataContext.cs
+++ Meal-Planner-Api/Meal-Planner-Api/Data/DataContext.cs
@@ -11,23 +11,68 @@ namespace Meal_Planner_Api.Data
 
         }
 
-        public DbSet<Amount> Amount { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<CookingTime> CookingTime { get; set; }
-        public DbSet<Instruction> Instruction { get; set; }
-        public DbSet<PreparationTime> PreparationTime { get; set; }
-        public DbSet<Rating> Rating { get; set; }
-        public DbSet<RecipeSchedule> RecipeSchedule { get; set; }
-        public DbSet<Unit> Unit { get; set; }
-        public DbSet<User> User { get; set; }
+        public DbSet<Amount> Amounts { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<CookingTime> CookingTimes { get; set; }
+        public DbSet<Instruction> Instructions { get; set; }
+        public DbSet<PreparationTime> PreparationTimes { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<RecipeSchedule> RecipeSchedules { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
-        public DbSet<Amount> Amounts { get; set; }
-        public DbSet<UserRecipeRating> UserRecipeRating { get; set; }
 
+        public DbSet<RecipeCookingTime> RecipeCookingTimes { get; set; }
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+        public DbSet<RecipePreparationTime> RecipePreparationTimes { get; set; }
+        public DbSet<RecipeRating> RecipeRatings { get; set; }
+        public DbSet<RecipeServings> RecipeServings { get; set; }
+ 
+
+        // Customize the tables
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
+
+
+            modelBuilder.Entity<RecipeCookingTime>()
+                .HasKey(rc => new { rc.RecipeID, rc.CookingTimeID });
+            modelBuilder.Entity<RecipeCookingTime>()
+                .HasOne(r => r.Recipe)
+                .WithMany(rc => rc.RecipeCookingTime)
+                .HasForeignKey(c => c.CookingTimeID);
+
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasKey(ri => new { ri.RecipeID, ri.IngredientID });
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(r => r.Recipe)
+                .WithMany(ri => ri.RecipeIngredients)
+                .HasForeignKey(i => i.IngredientID);
+
+
+            modelBuilder.Entity<RecipePreparationTime>()
+                .HasKey(rp => new {rp.RecipeID, rp.PreparationTimeID});
+            modelBuilder.Entity<RecipePreparationTime>()
+                .HasOne(r => r.Recipe)
+                .WithMany(rp => rp.RecipePreparationTime)
+                .HasForeignKey(p => p.PreparationTimeID);
+
+            modelBuilder.Entity<RecipeRating>()
+                .HasKey(rr => new {rr.RecipeID, rr.RatingID});
+            modelBuilder.Entity<RecipeRating>()
+                .HasOne(r => r.Recipe)
+                .WithMany(rr => rr.RecipeRating)
+                .HasForeignKey(r => r.RatingID);
+
+
+            modelBuilder.Entity<RecipeServings>()
+                .HasKey(rs => new {rs.RecipeID, rs.ServingID});
+            modelBuilder.Entity<RecipeServings>()
+                .HasOne(r => r.Recipe)
+                .WithMany(rs => rs.RecipeServings)
+                .HasForeignKey(s => s.ServingID);
 
         }
 
--- Meal-Planner-Api/Meal-Planner-Api/Models/RecipeCookingTime.cs
+++ Meal-Planner-Api/Meal-Planner-Api/Models/RecipeCookingTime.cs
@@ -3,7 +3,7 @@
     public class RecipeCookingTime
     {
         public int RecipeID { get; set; }
-        public int CooktinTimeID { get; set; }
+        public int CookingTimeID { get; set; }
         public Recipe Recipe { get; set; }
         public CookingTime CookingTime { get; set; }
 
