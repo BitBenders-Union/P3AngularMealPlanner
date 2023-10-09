18e515d734a0455146872dd74134abc65971e172
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2023-10-09 10:56:09 +0200

    Update AmountController.cs
    test2
    
    Co-Authored-By: Jonas Østergaard <98744422+jona339e@users.noreply.github.com>
    Co-Authored-By: ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>

--- Meal-Planner-Api/Meal-Planner-Api/Controllers/AmountController.cs
+++ Meal-Planner-Api/Meal-Planner-Api/Controllers/AmountController.cs
@@ -3,7 +3,7 @@ namespace Meal_Planner_Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AmountController : ControllerBase // Delete this
+    public class AmountController : ControllerBase // Delete this comment
     {
         private readonly IMapper _mapper;
         private readonly IAmountRepository _amountRepository;
